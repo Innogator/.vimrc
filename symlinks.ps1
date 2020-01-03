87030e3952a1ab874ada885d9dced712c8228ac0
Bryan <bryan.tanner@advent.com>
2020-01-03 15:37:57 -0500

    Add symlink powershell script

---
+++ symlinks.ps1
@@ -0,0 +1,4 @@
+### SYMBOLIC LINK FILES
+New-Item -ItemType SymbolicLink -Path C:\Users\btanner\.vimrc -Value C:\dev\vim\.vimrc
+New-Item -ItemType SymbolicLink -Path C:\Users\btanner\.vsvimrc -Value C:\dev\vim\.vsvimrc
+New-Item -ItemType SymbolicLink -Path C:\Users\btanner\AppData\Local\nvim\init.vim -Value C:\dev\vim\init.vim
